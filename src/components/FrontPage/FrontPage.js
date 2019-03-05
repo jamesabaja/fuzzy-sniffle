d0cf1dbba9e51711d2005eb59d5e43cb60e5b1d1
jgabaja <jgabaja@gmail.com>
2019-03-05 15:24:56 +0800

    wip

--- src/components/FrontPage/FrontPage.js
+++ src/components/FrontPage/FrontPage.js
@@ -29,10 +29,15 @@ class FrontPage extends Component {
             <b>Dr. Vena Pearl Bongolan</b>, <i>College of Engineering</i> <br/>
             <b>Dr. Rizalino B. Cruz</b>, <i>National College of Public Administration and Governance (NCPAG)</i> <br/>
             <b>Prof. Roselle Leah K. Rivera</b>, <i>College of Social Work and Community Development</i>
+
+            <br/>
+            <b>James Gabriel Abaja</b>, <i>BS Computer Science</i> <br/>
+            <b>Wystan Dmitri Concepcion</b>, <i>BS Computer Science</i> <br/>
           </p>
           <p>
           <b>Analysis is exclusively for the Philippine setting</b>, and individual respondents are <b>required a minimum of five years</b> in the practice or study of sustainability and development issues (may include years in graduate school). Foreign respondents are welcome if the Philippines is one of their study or practice areas, and have at least five years total experience (practice or study), including Philippine experience. 
           </p>
+          <p className='lead'><i>Chrome is the recommended browser for the software; we are working on compatibility issues with FireFox.</i></p>
           <hr className="my-2" />
           <p className="lead">
             <Button outline color='primary' href='/signup'>Signup </Button>{' '}
